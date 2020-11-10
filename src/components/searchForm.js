4293b518092faebc8afaa4137fc037b8a65cbcb6
shunpin tseng <shunpin@gmail.com>
2020-11-09 23:07:03 -0800

    search function

--- src/components/searchForm.js
+++ src/components/searchForm.js
@@ -1,35 +1,66 @@
 import React, { Component } from 'react';
-
+import API from "./utils/API"
 
  class SearchForm extends Component {
 
     state= {
-        name: "",        
+        searchName: "",
+        employees: [], 
+        sorted: []       
     };
+
+    componentDidMount() {
+        API.search()
+        .then (res => {
+            this.setState({
+                employees: res.data.results
+            });
+        })
+        .catch(err => console.log(err));
+    }
 
     handleInputchange = event => {
         let value = event.target.value;
-        let name = event.target.name
+        let searchName = event.target.searchName
+        this.setState({
+            [searchName]: value
+        })
     }
+
+    handleFormSubmit = event => {
+            event.preventDefault();
+            const searchName = this.state.searchName;
+            const result = this.state.employees.filter(employee => employee.name.first.includes(searchName) || employee.name.last.includes(searchName));
+            this.setState({
+                sorted: result,
+                searchName: ""
+            });
+        };
+    
+    
+
     render() {
-    return(<SearchForm>
-        <FormControl
-          onChange={props.handleInputChange}
-          value={props.value}
-          name="search"
-          type="text"
-          className="form-control"
-          placeholder="Search"
-          id="search"
-        />
+
+    return(
+   
+    <div className="form">
+
+    
+    <search
+          handleInputChange={this.handleInputChange}
+          search={this.state.searchName}
+    />
   
-        <Button
-          onClick={props.handleFormSubmit}
+        <button
+          onClick={this.handleFormSubmit}
           type="submit"
           className="button">
         Search
-        </Button> 
-        </SearchForm>     )
+        </button> 
+               
+        </div>
+        )
+         }
     }
-}
+
 export default SearchForm;
